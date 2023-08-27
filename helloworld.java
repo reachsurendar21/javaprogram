8200d82a5d8d835387d07a679e55a27daf3304bf
reachsurendar21 <142766771+reachsurendar21@users.noreply.github.com>
2023-08-26 23:08:23 -0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-     System.out.println("Surendar at the best");
+     System.out.println("Surendar, Malini, Ganishka & Yashika at the best");
     }
 }
